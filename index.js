5631c47f5f759923519f35da31a78ca65e4096b1
Lukas Varkalis <varkalis.lukas@gmail.com>
2019-01-07 14:30:22 +0200

    change node request to axios

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const q = require('q')
-const request = require('request')
+const axios = require('axios')
 const parse = require('./lib/parse')
 
 module.exports = function (url, options) {
@@ -8,7 +8,7 @@ module.exports = function (url, options) {
   const opts = {
     userAgent: options.userAgent || 'MetadataScraper',
     fromEmail: options.fromEmail || 'example@example.com',
-    maxRedirects: options.maxRedirects || 10,
+    maxRedirects: options.maxRedirects || 5,
     timeout: options.timeout || 10000,
     descriptionLength: options.descriptionLength || 750,
     ensureSecureImageRequest: options.ensureSecureImageRequest || true,
@@ -17,29 +17,29 @@ module.exports = function (url, options) {
   }
 
   const requestOpts = {
+    method: 'get',
     url: url,
     headers: {
       'User-Agent': opts.userAgent,
       'From': opts.fromEmail
     },
     maxRedirects: opts.maxRedirects,
-    encoding: 'utf8',
-    timeout: opts.timeout
-  }
-  request.get(requestOpts, function (err, response, body) {
-    if (err || !response) {
-      return dfd.reject(err)
-    }
-    if (response.statusCode && response.statusCode !== 200) {
-      return dfd.reject({ Error: 'response code ' + response.statusCode })
-    }
-    if (response.statusCode && response.statusCode === 200) {
-      // rewrite url if our request had to follow redirects to resolve the
-      // final link destination (for example: links shortened by bit.ly)
-      if (response.request.uri.href) url = response.request.uri.href
-      return dfd.resolve(parse(url, body, opts))
-    }
-  })
+    timeout: opts.timeout,
+  };
+
+  axios(requestOpts)
+    .then(response => {
+      if (response.statusCode && response.statusCode !== 200) {
+        return dfd.reject({ Error: 'response code ' + response.statusCode });
+      }
+
+      if (response.statusCode && response.statusCode === 200) {
+        return dfd.resolve(parse(url, response.data, opts));
+      }
+    })
+    .catch(err => {
+      return dfd.reject(err);
+    });
 
   return dfd.promise
 }
